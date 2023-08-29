7cd460e343a423f2a1a5c136ebdd881772996529
Dmitriy <plotnikov-d@bk.ru>
2023-08-29 22:44:11 +0300

    templates

--- realtys/admin.py
+++ realtys/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+import realtys.models
+
+
+class RealtyAdmin(admin.ModelAdmin):
+    list_display = ('type', 'cost', 'info')
+
+
 # Register your models here.
+
+admin.site.register(realtys.models.Realty, RealtyAdmin)
+admin.site.register(realtys.models.RealtyType)
+admin.site.register(realtys.models.RealtyManager)
