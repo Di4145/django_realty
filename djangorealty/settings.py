09c6ede2bccca7365452aa00fe69b4b10a10f167
Dmitriy Plotnikov <plotnikov-d@bk.ru>
2023-09-06 20:04:23 +0300

    search bu cost

--- djangorealty/settings.py
+++ djangorealty/settings.py
@@ -51,7 +51,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'realtys',
+    'django.contrib.humanize',
+
+    'realtys'
+
 ]
 
 MIDDLEWARE = [
--- realtys/views.py
+++ realtys/views.py
@@ -8,8 +8,23 @@ def index(request):
     return render(request, 'index.html', {'realty': realty})
 
 def search(request):
-    realtys = Realty.objects.all()
-    # realtys = Realty.objects.filter(info__iregex='балкон')
+    text_q = request.GET.get('text')
+    price1_q = request.GET.get('price1')
+    price2_q = request.GET.get('price2')
+
+    if text_q:
+        realtys = Realty.objects.filter(info__iregex=text_q)
+    else:
+        realtys = Realty.objects.all()
+
+    if price1_q:
+        realtys = realtys.filter(cost__gte=price1_q)
+
+    if price2_q:
+        realtys = realtys.filter(cost__lte=price2_q)
+
+
+
     return render(request, 'search.html', {'realtys': realtys})
 
 
